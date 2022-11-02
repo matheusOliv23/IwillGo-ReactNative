20f8dc7b3fcaf0bd8080915c1e4dc96ea0681ca9
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-02 08:10:32 -0300

    feat: adjusting input and button container styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,14 +10,16 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Quarta, 02 de Novembro de 2022</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do Participante"
-        placeholderTextColor="#6b6b6b"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
